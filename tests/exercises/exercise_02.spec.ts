11c64788aef7dded14c1bb0572503404f0b650f9
Stephen Meehan <smeehan@poppulo.com>
2025-06-26 16:33:10 +0100

    pom skeleton added

--- tests/exercises/exercise_02.spec.ts
+++ tests/exercises/exercise_02.spec.ts
@@ -1,1 +1,22 @@
 // Use the Page Object in a test
+
+// Create a Page Object Model for the login page
+import type { Page, Locator } from '@playwright/test';
+
+export class LoginPage {
+    private readonly userNameInput: Locator;
+
+
+    constructor(public readonly page: Page) {
+        this.userNameInput = this.page.getByRole('textbox', {name: 'Username'});
+
+    }
+
+    async goto() {
+        await this.page.goto('/login');
+    }
+
+    async login(username: string, password: string) {
+        this.goto();
+    }
+}
